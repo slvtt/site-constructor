b8757611c7be21e9537fb48caf7dbbbe3fcfbaab
slvtt <s.mavlikhanov@madbrains.ru>
2026-03-22 14:57:21 +0400

    init second time

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import WelcomeItem from 'src/components/WelcomeItem.vue'
-import DocumentationIcon from 'src/components/icons/IconDocumentation.vue'
-import ToolingIcon from 'src/components/icons/IconTooling.vue'
-import EcosystemIcon from 'src/components/icons/IconEcosystem.vue'
-import CommunityIcon from 'src/components/icons/IconCommunity.vue'
-import SupportIcon from 'src/components/icons/IconSupport.vue'
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
 
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import 'src/assets/main.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from 'src/App.vue'
-import router from 'src/router'
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
